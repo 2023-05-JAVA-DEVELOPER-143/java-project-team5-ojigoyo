f299985b6304eb257d0db55cdd512f28d9f8e29a
yukang <choliea91@gmail.com>
2023-07-20 06:54:38 +0900

    d

--- src/hotel/coupon/UserCoup.java
+++ src/hotel/coupon/UserCoup.java
@@ -8,4 +8,46 @@ public class UserCoup {
 private int userCoupNo;
 private List<User> userList;
 private List<Coupon> couponList;
+
+public UserCoup() {
+	// TODO Auto-generated constructor stub
+}
+
+public UserCoup(int userCoupNo, List<User> userList, List<Coupon> couponList) {
+	super();
+	this.userCoupNo = userCoupNo;
+	this.userList = userList;
+	this.couponList = couponList;
+}
+
+public int getUserCoupNo() {
+	return userCoupNo;
+}
+
+public void setUserCoupNo(int userCoupNo) {
+	this.userCoupNo = userCoupNo;
+}
+
+public List<User> getUserList() {
+	return userList;
+}
+
+public void setUserList(List<User> userList) {
+	this.userList = userList;
+}
+
+public List<Coupon> getCouponList() {
+	return couponList;
+}
+
+public void setCouponList(List<Coupon> couponList) {
+	this.couponList = couponList;
+}
+
+@Override
+public String toString() {
+	return "UserCoup [userCoupNo=" + userCoupNo + ", userList=" + userList + ", couponList=" + couponList + "]";
+}
+
+
 }
