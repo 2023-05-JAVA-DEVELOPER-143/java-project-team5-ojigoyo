f5093d1caa85e25cc633aa1c7c0eb13f9be44277
WooHanYeong <woocoding96@gmail.com>
2023-07-19 17:21:41 +0900

    ..

--- src/hotel/user/UserSQL.java
+++ src/hotel/user/UserSQL.java
@@ -1,15 +1,21 @@
 package hotel.user;
 
 public class UserSQL {
-	public static final String  USER_INSERT=
+	public static final String  USER_INSERT =
 			"insert into userinfo(USER_ID,USER_PASSWORD,USER_NAME,USER_EMAIL,USER_PHONE,USER_ADDRESS,USER_GENDER,USER_BIRTHDATE) "
 			+ "values(?,?,?,?,?,?,?,sysdate)";
 	
-	public static final String USER_SELECT_BY_ID="";
+	public static final String USER_UPDATE =
+			"update userinfo set USER_PASSWORD=?,USER_NAME=?,USER_EMAIL=?,USER_PHONE=?,USER_ADDRESS=?,USER_GENDER=? where USER_ID=?";
 	
-	public static final String USERT_REMOVE="";
+	public static final String USERT_REMOVE =
+			"delete from userinfo where USER_ID=?";
 	
-	public static final String USER_UPDATE="";
+	public static final String USER_SELECT_BY_ID=
+			"select USER_ID,USER_PASSWORD,USER_NAME,USER_EMAIL,USER_PHONE,USER_ADDRESS,USER_GENDER from userinfo where USER_ID=?";
 	
-	public static final String USER_SELECT_BY_ID_COUNT="";
+	
+	public static final String USER_SELECT_BY_ID_COUNT=
+			"select count(*) as cnt  from userinfo where USER_ID=?";
+
 }
