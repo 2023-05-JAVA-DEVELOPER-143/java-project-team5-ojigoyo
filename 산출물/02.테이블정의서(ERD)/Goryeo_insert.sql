17d91ce9ffeec8565ec7b5ebace06494fe458388
lsh96900410 <lsh96900410@gmail.com>
2023-07-19 18:20:26 +0900

    07.19

--- 산출물/02.테이블정의서(ERD)/Goryeo_insert.sql
+++ 산출물/02.테이블정의서(ERD)/Goryeo_insert.sql
@@ -3,7 +3,40 @@
 
 --room type insert
 
+
+insert into room_type (room_type_no,room_type_name,room_type_img,room_type_detail,room_type_pool,room_type_qty)
+        values (1,'1번객실타입','image1.jpg','1번객실타입 세부사항','F',2);
+
+insert into room_type
+(room_type_no,room_type_name,room_type_img,room_type_detail,room_type_pool,room_type_qty)
+	values	
+(2,'2번객실타입','image2.jpg','2번객실타입 세부사항','T',1);
+
+insert into room_type
+(room_type_no,room_type_name,room_type_img,room_type_detail,room_type_pool,room_type_qty)
+	values	
+(3,'3번객실타입','image3.jpg','3번객실타입 세부사항','T',0);
+
+
+
+
+
+
 --room insert
+
+insert into room(room_no,room_price,room_type_no,reserv_no) 
+	values (101,10,1,null);
+insert into room(room_no,room_price,room_type_no,reserv_no) 
+	values (102,10,1,null);
+insert into room(room_no,room_price,room_type_no,reserv_no) 
+	values (103,10,1,null);
+insert into room(room_no,room_price,room_type_no,reserv_no) 
+	values (201,100,2,null);
+insert into room(room_no,room_price,room_type_no,reserv_no) 
+	values (202,100,2,null);
+insert into room(room_no,room_price,room_type_no,reserv_no) 
+	values (301,1000,3,null);
+
 
 --inquiries insert
 
