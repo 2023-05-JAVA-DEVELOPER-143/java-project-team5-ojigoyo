045d2a14744b5892b06de3fbfec9abd79b30aa85
yukang <choliea91@gmail.com>
2023-07-19 17:56:15 +0900

    쿠폰,예약,히스토리 insert수정

--- 산출물/02.테이블정의서(ERD)/Goryeo_insert.sql
+++ 산출물/02.테이블정의서(ERD)/Goryeo_insert.sql
@@ -8,14 +8,14 @@
 --inquiries insert
 
 --reserv insert
-insert into reserv(reserv_check_in,reserv_check_out,reserv_adult,
+insert into reserv(reserv_no,reserv_check_in,reserv_check_out,reserv_adult,
 					reserv_child,isbreakfast,reserv_extra_bed,reserv_date,
 					reserv_fprice,reserv_payment,coupon_no,user_id) 
-			values(to_date('2022/02/22','YYYY/MM/DD'),to_date('2022/02/25','YYYY/MM/DD'),2,1,'T',0,sysdate,300000,'카드',1,1);
+			values(reserv_reserv_no_seq.nextval,to_date('2022/02/22','YYYY/MM/DD'),to_date('2022/02/25','YYYY/MM/DD'),2,1,'T',0,sysdate,300000,'카드',1,null);
 --history insert
-insert into history(review_no,reserv_no) values(1,1);
+insert into history(history_no,review_no,reserv_no) values(history_history_no_seq.nextval,null,null);
 --coupon insert
-insert into coupon(coupon_name,coupon_dc_rate) values('10%할인쿠폰',10);
+insert into coupon(coupon_no,coupon_name,coupon_dc_rate) values(coupon_coupon_no_seq.nextval,'10%할인쿠폰',10);
 --review insert
 
 --comment insert
