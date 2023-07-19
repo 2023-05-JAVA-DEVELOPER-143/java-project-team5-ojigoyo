de98d37d37dbbeeac3371fcb50a8feb23bc7180c
seo0ojin <seo0ojinnn@gmail.com>
2023-07-19 18:04:42 +0900

    2

--- 산출물/02.테이블정의서(ERD)/Goryeo_insert.sql
+++ 산출물/02.테이블정의서(ERD)/Goryeo_insert.sql
@@ -6,12 +6,14 @@
 --room insert
 
 --inquiries insert
+insert into inquiries(inquiries_no, inquiries_title, inquiries_content, inquiries_date) values(inquiries_inquiries_no_SEQ.nextval, '제목', '내용', sysdate);
 
 --reserv insert
 
 --history insert
 
 --review insert
+insert into review(review_no, review_date, review_title, review_content, review_img) values(review_review_no_SEQ.nextval, sysdate, '제목', '내용', 'img.jpg');
 
 --coupon insert
 
