22025891e05f4e1442d5683889c3692a58fcb8c7
yukang <choliea91@gmail.com>
2023-07-20 17:02:19 +0900

    reservSQL 완성

--- src/hotel/reserv/ReservSQL.java
+++ src/hotel/reserv/ReservSQL.java
@@ -1,9 +1,16 @@
 package hotel.reserv;
 
 public class ReservSQL {
-public static final String ISNERT_RESERV = "insert into reserv(reserv_no,reserv_check_in,reserv_check_out,reserv_adult,reserv_child,isbreakfast,reserv_extra_bed,reserv_date,reserv_fprice,reserv_payment,user_id) values(reserv_reserv_no_seq.nextval,to_date('2022/02/22','YYYY/MM/DD'),to_date('2022/02/25','YYYY/MM/DD'),2,1,'T',0,sysdate,300000,'카드',null)";
-public static final String UPDATE_RESERV_OPTION="update reserv set RESERV_ADULT=2,RESERV_CHILD=2,ISBREAKFAST='T',RESERV_EXTRA_BED=1,reserv_fprice=600000,RESERV_PAYMENT='card' where reserv_no=1";
+public static final String ISNERT_RESERV = "insert into reserv(reserv_no,reserv_check_in,reserv_check_out,reserv_adult,reserv_child,isbreakfast,reserv_extra_bed,reserv_date,reserv_fprice,reserv_payment,user_id) values(reserv_reserv_no_seq.nextval,to_date(?,'YYYY/MM/DD'),to_date(?,'YYYY/MM/DD'),?,?,?,?,sysdate,?,?,?)";
+public static final String UPDATE_RESERV_OPTION="update reserv set RESERV_ADULT=?,RESERV_CHILD=?,ISBREAKFAST=?,RESERV_EXTRA_BED=?,reserv_fprice=?,RESERV_PAYMENT=? where reserv_no=?";
 public static final String DELETE_RESERV_BY_USER_ID = "delete from reserv where reserv.user_id=?";
-public static final String SELECT_RESERV_BY_USER_ID = "select * from userinfo u join reserv r on u.user_id =r.user_id join user_coup uc on uc.user_id=u.user_id where u.user_id=?";
-public static final String SELECT_RESERV_ALL = "select * from userinfo u join reserv r on u.user_id  = r.user_id";
+public static final String DELETE_RESERV_BY_RESERV_NO= "delete from reserv where reserv_no=?";
+public static final String SELECT_ROOM_ROOMTYPE_ALL="select * from room r join room_type rt on r.room_type_no= rt.room_type_no";
+public static final String SELECT_ROOM_ROOMTYPE_RESERV_ALL="select * from room r join room_type rt on r.room_type_no= rt.room_type_no join reserv re on re.reserv_no=r.reserv_no";
+public static final String FIND_EMPTY_ROOM="select * from room r join room_type rt on r.room_type_no= rt.room_type_no join reserv re on re.reserv_no=r.reserv_no where rt.room_type_no=? and re.reserv_date>to_date(?,'YYYY/MM/DD') or re.reserv_date<to_date(?,'YYYY/MM/DD')";
+public static final String SELECT_RESERV_USER_ALL="select * from reserv re join userinfo u on u.user_id = re.user_id";
+public static final String SELECT_RESERV_USER_DETAIL="select * from reserv re join userinfo u on u.user_id = re.user_id join room r on re.reserv_no=r.reserv_no join room_type rt on r.room_type_no = rt.room_type_no where re.reserv_no=?";
+public static final String SELECT_RESERV_BY_ID="select * from reserv re join userinfo u on u.user_id = re.user_id where u.user_id=?";
+public static final String FIND_MY_RESERV_DEFAULT="select * from reserv re join userinfo u on u.user_id = re.user_id where u.user_id =? and (sysdate-reserv_date)<30";
+public static final String FIND_MY_RESERV_BY_DATE="select * from reserv re join userinfo u on u.user_id = re.user_id where u.user_id =? and reserv_date Between to_date(?,'yyyy/mm/dd') and to_date(?,'yyyy/mm/dd')+1";
 }
