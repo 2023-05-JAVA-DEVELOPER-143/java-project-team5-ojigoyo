649392e25077c3de04496b80c3ed2d48992bb80b
yukang <choliea91@gmail.com>
2023-07-20 20:54:09 +0900

    dfa

--- src/hotel/reserv/ReservDao.java
+++ src/hotel/reserv/ReservDao.java
@@ -1,6 +1,7 @@
 package hotel.reserv;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -69,24 +70,52 @@ public class ReservDao {
 		dataSource.close(con);
 		return rowCount;
 	}
-	public List<Room> selectRoomRTypeAll(){
-		List<Room> roomList = new ArrayList<Room>();
+//	public List<Room> selectRoomRTypeAll(){
+//		List<Room> roomList = new ArrayList<Room>();
+//		Connection con = dataSource.getConnection();
+//		PreparedStatement pstmt = con.prepareStatement(ReservSQL.SELECT_ROOM_ROOMTYPE_ALL);
+//		ResultSet rs = pstmt.executeQuery();
+//		while(rs.next()) {
+//			roomList.add(new Room(rs.getInt("room_no"), null, null))		
+//		}
+//		rs.close();
+//		pstmt.close();
+//		dataSource.close(con);
+//		return roomList;
+//		select * from room r join room_type rt on r.room_type_no= rt.room_type_no";
+//	}
+	public List<Reserv> selectAllReservRoom() throws Exception{
+		List<Reserv> reservList = new ArrayList<Reserv>();
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(ReservSQL.SELECT_ROOM_ROOMTYPE_ALL);
-		ResultSet rs = pstmt.executeQuery();
-		if(rs.next()) {
-			RoomType rt = new RoomType(rs.getInt("room_type_no"),rs.getString("room_type_name"),rs.getString("room_type_img"),rs.getString("room_type_detail"),rs.getInt("room_type_qty"),rs.getBoolean("room_type_pool"));
-					
+		PreparedStatement pstmt = con.prepareStatement(ReservSQL.SELECT_ROOM_ROOMTYPE_RESERV_ALL);
+		ResultSet rs =pstmt.executeQuery();
+		while(rs.next()) {
+			reservList.add(new Reserv(rs.getInt("reserv_no"), rs.getDate("reserv_check_in"), rs.getDate("reserv_check_out"), rs.getInt("reserv_adult"), rs.getInt("reserv_child"), rs.getBoolean("isbreakfast"), rs.getInt("reserv_extra_bed"), 
+					new Room(rs.getInt("room_no"),new RoomType(rs.getInt("room_type_no"), rs.getString("room_type_name"), rs.getString("room_type_img"), rs.getString("room_type_detail"), rs.getBoolean("room_type_pool"),rs.getInt("room_type_qty") , rs.getInt("room_type_price"), new ArrayList<Room>()),new ArrayList<Reserv>()), 
+					new User(), rs.getString("reserv_payment"), rs.getDate("reserv_date")));
 		}
 		rs.close();
 		pstmt.close();
 		dataSource.close(con);
-		return roomList;
-		select * from room r join room_type rt on r.room_type_no= rt.room_type_no";
+		return reservList;
+		}
+	public int emptyRoom(int roomTypeNo,Date checkIn, Date checkOut) throws Exception {
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ReservSQL.FIND_EMPTY_ROOM);
+		pstmt.setInt(1, roomTypeNo);
+		pstmt.setDate(2, checkIn);
+		pstmt.setDate(3, checkOut);
+		ResultSet rs =pstmt.executeQuery();
+		int emptyRoomNo=0;
+		if(rs.next()) {
+			 emptyRoomNo = rs.getInt("room_no");
+		}
+		rs.close();
+		pstmt.close();
+		dataSource.close(con);
+		return emptyRoomNo;
 	}
-	public static final String SELECT_ROOM_ROOMTYPE_RESERV_ALL="select * from room r join room_type rt on r.room_type_no= rt.room_type_no join reserv re on re.reserv_no=r.reserv_no";
-	public static final String FIND_EMPTY_ROOM="select * from room r join room_type rt on r.room_type_no= rt.room_type_no join reserv re on re.reserv_no=r.reserv_no where rt.room_type_no=? and re.reserv_date>to_date(?,'YYYY/MM/DD') or re.reserv_date<to_date(?,'YYYY/MM/DD')";
-	public static final String SELECT_RESERV_USER_ALL="select * from reserv re join userinfo u on u.user_id = re.user_id";
+	public static final String SELECT_RESERV_USER_ALL="select * from reserv";
 	public static final String SELECT_RESERV_USER_DETAIL="select * from reserv re join userinfo u on u.user_id = re.user_id join room r on re.reserv_no=r.reserv_no join room_type rt on r.room_type_no = rt.room_type_no where re.reserv_no=?";
 	public static final String SELECT_RESERV_BY_ID="select * from reserv re join userinfo u on u.user_id = re.user_id where u.user_id=?";
 	public static final String FIND_MY_RESERV_DEFAULT="select * from reserv re join userinfo u on u.user_id = re.user_id where u.user_id =? and (sysdate-reserv_date)<30";
